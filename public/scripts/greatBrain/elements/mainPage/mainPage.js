e479cebb5f55c119202ee8670d436cfd3f3738ef
Alexander Kushko <kushko.alex@gmail.com>
2015-09-01 01:26:13 +0300

    main page and common elements dev v.0.0.7

--- public/scripts/greatBrain/elements/mainPage/mainPage.js
+++ public/scripts/greatBrain/elements/mainPage/mainPage.js
@@ -1,14 +1,110 @@
 GB.mainPage = function ($parent) {
     var gb = this,
         global = gb.global,
-        a9=global.A9,
+        a9 = global.A9,
         tp = global.cnCt.tp,
+        settings = gb.settings,
+        doc = global.document,
+        eventOnPointerEnd = a9.deviceInfo.eventOnPointerEnd,
+        startY,
+        finishY,
+        startT,
+        finishT,
+        duration = gb.settings.scrollToAnnouncementsDuration || 200,
         build,
-        $feedbackFormWrapper;
+        buildBanner,
+        mainPageData = settings.dataModels.mainPage,
+        $feedbackFormWrapper,
+        mainPageSlideTimeout= settings.controlsDescriptors.site.mainPageSlideTimeout | 3000,
+        $fragment,
+        currentVisibleFrameIndex = -1,
+        i,
+        $mainImageContentWrapper,
+        $contentImages = [];
 
 
-     build = tp('mainPage', $parent);
-     $feedbackFormWrapper = build.feedbackFormWrapper;
-     gb.feedbackForm($feedbackFormWrapper);
+    build = tp('mainPage', $parent);
+    $mainImageContentWrapper = build.mainImageContentWrapper;
+
+    $fragment = global.document.createDocumentFragment();
+
+    var j=0;
+    a9.each(mainPageData.mainBanners, function(mainBanner){
+        mainBanner.order=j;
+        buildBanner = tp('mainImageContent', mainBanner, $fragment);
+        $contentImages.push(buildBanner.r);
+        j++;
+    });
+
+    var sm = tp('showMore',$fragment);
+
+    a9.addEvent(sm.r, eventOnPointerEnd, showDetails);
+
+    function showDetails() {
+        scrollToTop();
+    }
+
+    function scrollToTop() {
+        finishY = (a9.$('mainPageAnnouncements').offsetTop || 0);
+        //finishY = (a9.$('contentBlock'+index).offsetTop || 0);
+        startY = ((global.pageYOffset || doc.scrollTop) || 0) - (doc.clientTop || 0);
+        startT = +(new Date());
+        finishT = startT + duration;
+        setTimeout(animate, 15);
+    }
+
+    function animate() {
+        var now = +(new Date()),
+            shift = (now > finishT) ? 1 : (now - startT) / duration;
+        global.scrollTo(0, interpolate(startY, finishY, easing(shift)));
+        (now > finishT) || setTimeout(animate, 15);
+    }
+
+    function interpolate(source, target, shift) {
+        return (source + ((target - source) * shift));
+    }
+
+    function easing(pos) {
+        return (-Math.cos(pos * Math.PI) / 2) + .5;
+    }
+
+    $mainImageContentWrapper.appendChild($fragment);
+
+
+    $feedbackFormWrapper = build.feedbackFormWrapper;
+    gb.feedbackForm($feedbackFormWrapper);
+
+
+    setTimeout(slideImageFrame, 10);
+
+    function setInactiveImages() {
+        //console.log($contentImages[0]);
+        for (i = 0; i < $contentImages.length; i++) {
+            a9.removeClass($contentImages[i], "active");
+        }
+    }
+
+    function updateCurrentVisibleFrameIndex() {
+        currentVisibleFrameIndex++;
+        if (currentVisibleFrameIndex >= $contentImages.length) {
+            currentVisibleFrameIndex = 0;
+        }
+    }
+
+    function setActiveImage() {
+        for (i = 0; i < $contentImages.length; i++) {
+            if (i == currentVisibleFrameIndex) {
+                a9.addClass($contentImages[i], "active");
+            }
+        }
+    }
+
+    function slideImageFrame() {
+        setInactiveImages();
+        updateCurrentVisibleFrameIndex();
+        setActiveImage();
+        setTimeout(slideImageFrame, mainPageSlideTimeout);
+    }
+
 
 };
--- public/scripts/greatBrain/elements/mainPage/tmpls_mainPage.js
+++ public/scripts/greatBrain/elements/mainPage/tmpls_mainPage.js
@@ -1,15 +1,108 @@
-(function(gb){
+(function (gb) {
     var tmpls = gb.tmpls,
         a9 = gb.global.A9,
         l10n = a9.l10n,
         u;
 
-    tmpls.mainPage = function(){
+    tmpls.mainPage = function () {
         return [
-            {c:'main-banner'},
+            {c: 'main-banner', n: 'mainImageContentWrapper'},
+            tmpls.mainPageAnnouncements(),
             tmpls.header(),
             tmpls.footer()
         ]
     };
+
+    tmpls.mainPageAnnouncements = function () {
+        var announcements = gb.settings.dataModels.mainPage.contentAnnouncements,
+            parallaxImages = gb.settings.dataModels.mainPage.parallaxImages,
+            content = [],
+            i = 0;
+        a9.each(announcements, function (announcement) {
+            if (i % 2 == 0) {
+                content.push([tmpls.announcementInfo(announcement), tmpls.announcementImage(announcement)]);
+            }
+            else {
+                content.push([tmpls.announcementImage(announcement), tmpls.announcementInfo(announcement)]);
+            }
+
+            content.push({c: 'clear'});
+
+            if(parallaxImages[i]){
+                content.push({c: 'parallax',C:{e:'img',a:{src:gb.settings.controlsDescriptors.site.mainPageImages + parallaxImages[i]}}});
+            }
+
+            content.push({c: 'clear'});
+            i++;
+        });
+
+        return {a: {id: 'mainPageAnnouncements'}, c: 'main-page-announcements', C: content}
+    };
+
+
+    tmpls.announcementInfo = function (announcement) {
+        return {
+            c: 'announcement-info', C: [
+                {c: 'title',t:announcement.title},
+                {c:'logo'},
+                {c: 'text',t:announcement.text},
+                {e:'a', h:announcement.url, c:'link', t:l10n('announcementLinkText')}
+            ]
+        }
+    };
+
+    tmpls.announcementImage = function (announcement) {
+        return {
+            c: 'announcement-image',
+            e: 'img',
+            a: {src: gb.settings.controlsDescriptors.site.mainPageImages + announcement.imageSrc}
+        }
+    };
+
+
+    tmpls.mainImageContent = function (mainBanner) {
+        var controlsDescriptors = gb.settings.controlsDescriptors,
+            content = [];
+
+
+        if (mainBanner.title && mainBanner.title != '') {
+            content.push({c: 'title', H: mainBanner.title});
+            content.push({c: 'clear'})
+        }
+
+        if (mainBanner.description && mainBanner.description != '') {
+            content.push({c: 'description', C: {e: 'span', c: 'highlight', H: mainBanner.description}});
+            content.push({c: 'clear'})
+        }
+
+        if (mainBanner.sign && mainBanner.sign != '') {
+            content.push({c: 'sign', H: mainBanner.sign});
+            content.push({c: 'clear'})
+        }
+
+        if (mainBanner.signImage && mainBanner.signImage != '') {
+            content.push({
+                c: 'sign-image',
+                C: {e: 'img', a: {src: controlsDescriptors.site.mainPageImages + mainBanner.signImage}}
+            });
+            content.push({c: 'clear'})
+        }
+
+        return {
+            c: 'main-image-content', C: [
+                {
+                    e: 'img', c: 'banner', a: {src: controlsDescriptors.site.mainPageImages + mainBanner.imageSrc}
+                },
+                {
+                    c: 'main-page-info-wrapper', C: content
+                }
+            ]
+        }
+    };
+
+
+    tmpls.showMore = function () {
+        return {c: 'show-more', H: l10n('showMoreText')}
+    }
 
 }(GB));
