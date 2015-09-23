78e0c1cdf39bfe0bb1eeec96da5a2f5d0b4c52ba
Alexander Kushko <kushko.alex@gmail.com>
2015-09-24 00:08:28 +0300

    v.0.9.4 intro animation fixed

--- public/scripts/greatBrain/elements/intro/intro.js
+++ public/scripts/greatBrain/elements/intro/intro.js
@@ -1,4 +1,4 @@
-GB.intro = function($parent){
+GB.intro = function ($parent) {
     var gb = this,
         global = gb.global,
         a9 = global.A9,
@@ -26,55 +26,52 @@ GB.intro = function($parent){
     var $mazeLayoutTextSmall = build.mazeLayoutTextSmall;
     var $intro = build.intro;
     var $skipIntroWrapperButton = build.skipIntroWrapperButton;
-var $finalLayout = build.finalLayout;
+    var $finalLayout = build.finalLayout;
     //a9.removeClass($mazeContainer,'hidden');
     //a9.animate($mazeShutter,['width',0,'px', 'height',0,'px','margin-top',255,'px'],2000,step5);
 
 
     //setTimeout(step5,3000);
 
-    setTimeout(step1,2000);
+    setTimeout(step1, 2000);
 
-    function step1(){
-        a9.animate($asUsual,['margin-top', 10, 'px', 'font-size',20,'px'], 600, step2);
+    function step1() {
+        a9.animate($asUsual, ['margin-top', 10, 'px', 'font-size', 20, 'px'], 600, step2);
     }
 
-    function step2(){
-        setTimeout(function(){
-            a9.removeClass($child,'hidden');
-            setTimeout(function(){
-                a9.removeClass($edu,'hidden');
-                a9.removeClass($mazeContainer,'hidden');
-                setTimeout(function(){
-                    a9.animate($mazeShutter,['width',0,'px', 'height',0,'px','margin-top',255,'px'],2000,step5);
-                },1000);
-            },1000);
-        },1000);
+    function step2() {
+        setTimeout(function () {
+            a9.removeClass($child, 'hidden');
+            setTimeout(function () {
+                a9.removeClass($edu, 'hidden');
+                a9.removeClass($mazeContainer, 'hidden');
+                setTimeout(function () {
+                    a9.animate($mazeShutter, ['width', 0, 'px', 'height', 0, 'px', 'margin-top', 255, 'px'], 2000, step5);
+                }, 1000);
+            }, 1000);
+        }, 1000);
     }
 
-    function step5(){
-        setTimeout(function(){
-            a9.removeClass($mazeLayout,'hidden');
-            a9.addClass($asUsual,'hidden');
-            setTimeout(function(){
-                a9.removeClass($mazeLayoutTextSmall,'hidden');
-                a9.addClass($mazeLayout,'hidden');
-                setTimeout(function(){
-                    a9.removeClass($whiteLine,'hidden');
-                    setTimeout(function(){
-                        a9.removeClass($greenLine,'hidden');
-                        setTimeout(function(){
-                            a9.animate($greenLine,['width',855,'px'],1000,function(){
-                                setTimeout(function(){
-                                    a9.addClass($intro,'hidden');
-                                    a9.addClass($skipIntroWrapperButton,'hidden');
-                                    a9.removeClass($finalLayout,'hidden');
-                                },2000);
-                            });
-                        },500);
-                    },1000)
-                },1000);
-            },3000);
-        },1000);
+    function step5() {
+        setTimeout(function () {
+            a9.removeClass($mazeLayout, 'hidden');
+            a9.addClass($asUsual, 'hidden');
+            setTimeout(function () {
+                a9.removeClass($mazeLayoutTextSmall, 'hidden');
+                a9.addClass($mazeLayout, 'hidden');
+                setTimeout(function () {
+                    a9.removeClass($whiteLine, 'hidden');
+                    a9.removeClass($greenLine, 'hidden');
+                    setTimeout(function () {
+                        setTimeout(function () {
+                            a9.addClass($intro, 'hidden');
+                            a9.addClass($skipIntroWrapperButton, 'hidden');
+                            a9.removeClass($finalLayout, 'hidden');
+                        }, 2000);
+                    }, 500);
+
+                }, 1000);
+            }, 3000);
+        }, 1000);
     }
 };
