1786bdb961f01f27b05bf915c33275737ccf26dc
Alexander Kushko <kushko.alex@gmail.com>
2015-09-25 01:28:46 +0300

    v.0.9.7 dropdown

--- public/scripts/a9/UIComponents/dropdown.js
+++ public/scripts/a9/UIComponents/dropdown.js
@@ -1,8 +1,26 @@
+(function (gb,a9) {
+    var tmpls = gb.tmpls,
+        l10n = a9.l10n,
+
+        u;
+
+
+    tmpls.dropdown = function(){
+
+        return{c:'drop-down-list',C:{c:'wrapper'}}
+    }
+
+}(GB,A9));
+
+
 (function (a9) {
-    a9.dropdown = function(object,options){
+    a9.dropdown = function(object, items, options){
         var $items,
+            build,
             eventOnPointerEnd = a9.deviceInfo.eventOnPointerEnd;
 
+
+        build = tp('blogDetails', newsData, $parent);
 
 
     }
