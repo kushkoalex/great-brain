775b4c8a8ce04237ecace74cbd15d2355d85d05c
Alexander Kushko <kushko.alex@gmail.com>
2015-10-06 00:44:01 +0300

    v.0.9.17 map location info panel fixes

--- public/scripts/greatBrain/elements/map/map.js
+++ public/scripts/greatBrain/elements/map/map.js
@@ -5,17 +5,20 @@ GB.map = function ($map, $mapLocationLinks) {
         tp = global.cnCt.tp,
         settings = gb.settings,
         eventOnPointerEnd = a9.deviceInfo.eventOnPointerEnd,
-        $buildLocationLinks=[],
+        $buildLocationLinks = [],
         $fragment;
 
 
     function initMap(index) {
-        index = index||0;
-        var location  = settings.dataModels.mapLocations[index];
+        index = index || 0;
+        var location = settings.dataModels.mapLocations[index];
         var lat = location.location.lat;
         var lng = location.location.lng;
 
         //console.log(location);
+
+
+        var contentInfo = '<div class="map-location-info-content"><div class="address">' + location.location.contentAddress + '</div><div class="phone">' + location.location.contentPhone + '</div><div class="email"><a href="mailto:' + location.location.contentEmail + '">' + location.location.contentEmail + '</a></div></div>'
 
         google.load('maps', '3', {
             callback: function () {
@@ -27,7 +30,7 @@ GB.map = function ($map, $mapLocationLinks) {
                 };
                 var map = new google.maps.Map($map, mapOptions);
                 var infoWindow = new google.maps.InfoWindow({
-                    content: location.location.content
+                    content: contentInfo
                 });
                 var marker = new google.maps.Marker({
                     position: coordinates,
@@ -50,10 +53,10 @@ GB.map = function ($map, $mapLocationLinks) {
         }
 
         a9.addEvent($buildLocationLinks[index], eventOnPointerEnd, function () {
-            a9.each($buildLocationLinks,function(buildLocationLink){
-                a9.removeClass(buildLocationLink,'active');
+            a9.each($buildLocationLinks, function (buildLocationLink) {
+                a9.removeClass(buildLocationLink, 'active');
             });
-            a9.addClass(this,'active');
+            a9.addClass(this, 'active');
             initMap(index);
         });
 
