a09bcb9eee506f907be6c51ee295d712d238c587
Alexander Kushko <kushko.alex@gmail.com>
2015-10-29 23:42:16 +0200

    fixes

---
+++ public/scripts/greatBrain/elements/educationKindsMobile/tmpls_educationKindsMobile.js
@@ -0,0 +1,11 @@
+(function (gb) {
+    var tmpls = gb.tmpls,
+        a9 = gb.global.A9,
+        l10n = a9.l10n,
+        u;
+
+    tmpls.educationKindsMobile = function(pageData){
+
+    }
+
+}(GB));
